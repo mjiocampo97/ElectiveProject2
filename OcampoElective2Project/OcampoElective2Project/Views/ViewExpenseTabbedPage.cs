fc2cd8e71b05fac31337ef1e81b7e9e15f621885
Mark <ocampomarkjohn999@gmail.com>
2019-03-13 10:46:55 +0800

    added class for tabbed page

---
+++ OcampoElective2Project/OcampoElective2Project/Views/ViewExpenseTabbedPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace OcampoElective2Project.Views
+{
+    public class ViewExpenseTabbedPage : TabbedPage
+    {
+        public ViewExpenseTabbedPage()
+        {
+            Title = "Expenses";
+            Children.Add(new ClothesPage());
+            Children.Add(new FoodPage());
+            Children.Add(new TransportationPage());
+            Children.Add(new OthersPage());
+
+        }
+    }
+}
