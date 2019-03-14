03b433dd7bfa36c30891beca0673906f649ea64f
Mark <ocampomarkjohn999@gmail.com>
2019-03-14 12:02:51 +0800

    add picture and add homepage ui

--- OcampoElective2Project/OcampoElective2Project.Android/Resources/Resource.designer.cs
+++ OcampoElective2Project/OcampoElective2Project.Android/Resources/Resource.designer.cs
@@ -2207,26 +2207,26 @@ namespace OcampoElective2Project.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020112
-			public const int avd_hide_password_1 = 2130837778;
-			
-			// aapt resource value: 0x7f020113
-			public const int avd_hide_password_2 = 2130837779;
-			
 			// aapt resource value: 0x7f020114
-			public const int avd_hide_password_3 = 2130837780;
+			public const int avd_hide_password_1 = 2130837780;
+			
+			// aapt resource value: 0x7f020115
+			public const int avd_hide_password_2 = 2130837781;
+			
+			// aapt resource value: 0x7f020116
+			public const int avd_hide_password_3 = 2130837782;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020115
-			public const int avd_show_password_1 = 2130837781;
-			
-			// aapt resource value: 0x7f020116
-			public const int avd_show_password_2 = 2130837782;
-			
 			// aapt resource value: 0x7f020117
-			public const int avd_show_password_3 = 2130837783;
+			public const int avd_show_password_1 = 2130837783;
+			
+			// aapt resource value: 0x7f020118
+			public const int avd_show_password_2 = 2130837784;
+			
+			// aapt resource value: 0x7f020119
+			public const int avd_show_password_3 = 2130837785;
 			
 			// aapt resource value: 0x7f020056
 			public const int clothes = 2130837590;
@@ -2774,11 +2774,11 @@ namespace OcampoElective2Project.Droid
 			// aapt resource value: 0x7f02010b
 			public const int notification_icon_background = 2130837771;
 			
-			// aapt resource value: 0x7f020110
-			public const int notification_template_icon_bg = 2130837776;
+			// aapt resource value: 0x7f020112
+			public const int notification_template_icon_bg = 2130837778;
 			
-			// aapt resource value: 0x7f020111
-			public const int notification_template_icon_low_bg = 2130837777;
+			// aapt resource value: 0x7f020113
+			public const int notification_template_icon_low_bg = 2130837779;
 			
 			// aapt resource value: 0x7f02010c
 			public const int notification_tile_bg = 2130837772;
@@ -2787,10 +2787,16 @@ namespace OcampoElective2Project.Droid
 			public const int notify_panel_notification_icon_bg = 2130837773;
 			
 			// aapt resource value: 0x7f02010e
-			public const int trans = 2130837774;
+			public const int profile = 2130837774;
 			
 			// aapt resource value: 0x7f02010f
-			public const int uti = 2130837775;
+			public const int profile2 = 2130837775;
+			
+			// aapt resource value: 0x7f020110
+			public const int trans = 2130837776;
+			
+			// aapt resource value: 0x7f020111
+			public const int uti = 2130837777;
 			
 			static Drawable()
 			{
