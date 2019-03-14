bf7004ab968d3e4eef10f616e94f187f768f4825
Mark <ocampomarkjohn999@gmail.com>
2019-03-14 15:00:16 +0800

    added logic command in income  viewmodel

--- OcampoElective2Project/OcampoElective2Project/ViewModels/IncomeViewModel.cs
+++ OcampoElective2Project/OcampoElective2Project/ViewModels/IncomeViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Views;
 using OcampoElective2Project.Helpers;
 using OcampoElective2Project.Models;
@@ -11,6 +13,8 @@ using OcampoElective2Project.Services.FoodService;
 using OcampoElective2Project.Services.IncomeService;
 using OcampoElective2Project.Services.OthersService;
 using OcampoElective2Project.Services.TransportationService;
+using OcampoElective2Project.Views;
+using Xamarin.Forms;
 
 namespace OcampoElective2Project.ViewModels
 {
@@ -31,6 +35,7 @@ namespace OcampoElective2Project.ViewModels
                 RaisePropertyChanged(nameof(User));
             }
         }
+        public bool isUpdate { get; set; }
 
         public IIncomeService IncomeService { get; set; }
         public ObservableCollection<Income> IncomeList { get; set; } = new ObservableCollection<Income>();
@@ -52,6 +57,122 @@ namespace OcampoElective2Project.ViewModels
             NavigationService = (NavigationService)navigationService;
             IncomeService = incomeService;
         }
-       
+
+
+
+        public void LoadIncome(UserAccount user)
+        {
+            IncomeList.Clear();
+            foreach (var  income in IncomeService.GetIncomeUser(User))
+                 {
+                        IncomeList.Add(income);
+                 }
+        }
+
+        public ICommand AddIncomeCommand=> new RelayCommand(AddIncomeProc);
+
+        private void AddIncomeProc()
+        {
+            NavigationService.NavigateTo(ViewModelLocator.AddIncomePage, User, false);
+        }
+
+        public ICommand DeleteIncomeCommand => new RelayCommand(DeleteIncomeProc);
+
+        private void DeleteIncomeProc()
+        {
+            if (SelectedIncome != null)
+            {
+
+                IncomeService.DeleteIncome(SelectedIncome);
+                RefreshIncome();
+            }
+            else
+            {
+
+                Application.Current.MainPage.DisplayAlert("Error",
+                    "Please Select an Income that you want to be deleted", "Cancel");
+
+            }
+
+        }
+
+        public ICommand UpdateIncomeCommand => new RelayCommand(UpdateIncomeProc);
+
+        private void UpdateIncomeProc()
+        {
+            if (SelectedIncome != null)
+            {
+                isUpdate = true;
+                NavigationService.NavigateTo(ViewModelLocator.AddIncomePage,User,false);
+            }
+            else
+            {
+                Application.Current.MainPage.DisplayAlert("Error", "Please Select an Income that you want to be updated", "Cancel");
+            }
+
+        }
+
+        public void RefreshIncome()
+        {
+            IncomeList.Clear();
+            foreach (var income in IncomeService.GetIncomeUser(User))
+            {
+                IncomeList.Add(income);
+            }
+        }
     }
+
+
+//    public ICommand DeleteClothesCommand => new RelayCommand(DeleteClothesProc);
+
+//    public void DeleteClothesProc()
+//    {
+//    if (SelectedClothes != null)
+//    {
+//    ClothesService.DeleteClothes(SelectedClothes);
+//    Refresh();
+//    }
+//    else
+//    {
+//    Application.Current.MainPage.DisplayAlert("Error", "Please Select a clothe that you want to be deleted", "Cancel");
+//    }
+
+//}
+
+//public void Refresh()
+//{
+//ClothesList.Clear();
+//foreach (var clothes in ClothesService.GetClothesUser(User))
+//{
+//    ClothesList.Add(clothes);
+//}
+//}
+
+
+//public ICommand AddClothesCommand => new RelayCommand(AddClothesProc);
+
+//private void AddClothesProc()
+//{
+//NavigationService.NavigateTo(ViewModelLocator.AddClothesPage, User, false);
+
+//}
+
+//public bool isUpdate { get; set; }
+//public ICommand UpdateClothesCommand => new RelayCommand(UpdateClothesProc);
+
+//private void UpdateClothesProc()
+//{
+//if (SelectedClothes != null)
+//{
+//isUpdate = true;
+//NavigationService.NavigateTo(ViewModelLocator.AddClothesPage, User, false);
+
+//}
+//else
+//{
+//Application.Current.MainPage.DisplayAlert("Error", "Please Select a clothe that you want to be updated", "Cancel");
+//}
+
+//return;
+
 }
